43f9e68ab0057dda8fca85f578363825a13131f8
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-18 10:21:46 -0600

    Changes color - things are doing things

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   // have fun!
+  let $topWord = $('h1');
+  $topWord.on('click',() => {
+    $topWord.css('color','orange');
+  });
+  let $button = $('button');
+  $button.on('click',() => {
+    $topWord.css('color','green');
+  });
 })
