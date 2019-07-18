d425e4205154c7a68467a9bb40e4f704c7ac0b94
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-18 10:41:30 -0600

    Prints bluecrab - word from top_word GET

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   // have fun!
+
+  function getTopWord() {
+    const word = ""
+    fetch("https://wordwatch-api.herokuapp.com/api/v1/top_word")
+      .then(response => {
+        return response.json()
+      })
+      .then(jsonResponse => {
+        const word = Object.keys(jsonResponse["word"])[0]
+        $('h1').text(word)
+      })
+  }
+  console.log(getTopWord())
+
+
+
   let $topWord = $('h1');
   $topWord.on('click',() => {
     $topWord.css('color','orange');
